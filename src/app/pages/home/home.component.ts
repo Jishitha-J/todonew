479e7ded0d9e59ab0bc370f6987d73237ca27277
Jishitha-J <jishithajayakumar486@gmail.com>
2023-11-22 17:22:01 +0530

    first commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,40 @@
+import { ApplicationConfig, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../units/navbar/navbar.component';
+import { ApiService } from '../../api.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule,NavbarComponent,FormsModule],
+  templateUrl:'./home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  [x: string]: any;
+    inputAdd!: string;
+    items: any;
+    
+  constructor(private api: ApiService) { }
+    todos: any;
+    newTaskTitle: string = '';
+      
+        ngOnInit(){
+          this.api.getTodo().subscribe((data:any) => {
+            this.todos=data
+          })
+  
+        }
+        delete(id: any) {
+          this.todos = this.todos.filter((e : {id:any})=> e.id != id)
+        }
+        addNewTask() {
+          if (this.newTaskTitle.trim() !== '') {
+            const newTask = { title: this.newTaskTitle, finished: false };
+            this.todos.push(newTask);
+            this.newTaskTitle = ''; // Clear the input field after adding the task
+          }
+        }
+      
+      }
